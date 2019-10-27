6a366b5356f22d262cfe414dff8886c52b833d31
Md. Jaid Bin Hashem <jaid.hashem@bs-23.net>
2019-10-27 16:45:42 +0600

    Running from MacBook

--- metro.config.js
+++ metro.config.js
@@ -10,7 +10,7 @@ const blacklist = require('metro-config/src/defaults/blacklist');
 module.exports = {
   resolver:{
     blacklistRE: blacklist([
-      /nodejs-assets\/.*/,
+      /node_modules\/.*\/node_modules\/react-native\/.*/,
       /android\/.*/,
       /ios\/.*/
     ])
